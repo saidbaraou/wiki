e6df6e9a20df6bc010fda45e01efa09338d0dda1
Said <baraoudev@gmail.com>
2024-11-26 12:41:53 +0100

    Defined a error page

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -9,11 +9,11 @@ Function that converts md files to HTML
 """
 def md_conversion(title):
     md_page = util.get_entry(title)
-    markdowner = Markdown()
+    # markdowner = Markdown()
     if md_page == None:
         return None
     else:
-        return markdowner.convert(md_page)
+        return Markdown().convert(md_page)
     
 
 def index(request):
