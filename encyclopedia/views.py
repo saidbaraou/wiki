3052719d075668796ccd800de3cac15ae0e7cc25
Said <baraoudev@gmail.com>
2024-11-26 12:26:53 +0100

    import markdown and defined a function to convert md to html

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,8 +1,17 @@
 # from django.http import HttpResponse
 from django.shortcuts import render
+import markdown
 
 from . import util
 
+def md_conversion(title):
+    md_page = util.get_entry(title)
+    markdowner = Markdown()
+    if md_page == None:
+        return None
+    else:
+        return markdowner.convert(md_page)
+    
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
