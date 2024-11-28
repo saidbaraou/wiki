8a01d8ba3a57a181f17f84e6c4351b3884e433f6
Said <baraoudev@gmail.com>
2024-11-28 09:25:34 +0100

    feat: add links on unordered list

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 
 from . import views
 
+
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:title>", views.entry_page, name="entry_page"),
+    path("wiki/<str:title>/", views.entry_page, name="entry_page"),
 ]
