0203ecbdb4213bab481c74000a59d0cd4296ad44
Said <baraoudev@gmail.com>
2024-12-11 13:21:57 +0100

    add a path to random page in urls

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path("search/", views.search, name="search"),
     path("new-page/", views.new_page, name="new_page"),
     path("edit-content/", views.edit_content, name="edit_content"),
-    path("save-changes/", views.save_changes, name="save_changes")
+    path("save-changes/", views.save_changes, name="save_changes"),
+    path("wiki/<str:title>/", views.random_page, name="random_page")
 ]
