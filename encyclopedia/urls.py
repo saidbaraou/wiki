4014a39197fd64618e4e5168a1e4782f328b0795
Said <baraoudev@gmail.com>
2025-07-13 21:45:03 +0200

    fix: change edit content path in urls.py
    Changed edit-content to edit/<str:title>/
    Did it to use it to display the edit from Get request as passing the title as part of the URL.

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path("wiki/<str:title>/", views.entry_page, name="entry_page"),
     path("search/", views.search, name="search"),
     path("new-page/", views.new_page, name="new_page"),
-    path("edit-content/", views.edit_content, name="edit_content"),
+    path("edit/<str:title>/", views.edit_content, name="edit_content"),
     path("save-changes/", views.save_changes, name="save_changes"),
     path("random-page/", views.random_page, name="random_page")
 ]
