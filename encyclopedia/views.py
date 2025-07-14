2545d22d826e66b70217d20d4e86be849659593b
Said <baraoudev@gmail.com>
2025-07-14 22:38:32 +0200

    fix: edit edit-content and save-changes functions
    Edited the two functions for 2 reasons :
    - edit-content to fetch the full Markdown, strips the #Mytitle line and renders the edit-content.html, passing only the body to the content variable for the textarea
    -save-changes to send a post request to /save_changes/ and prepends #{title}\n\n to correctly reconstruct the full Markdown with a single H1 title

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,6 +1,7 @@
 # from django.http import HttpResponse
 from django.shortcuts import render
-import markdown, random
+import markdown, random, redirect
+from django.http import Http404
 from . import util
 
 """
