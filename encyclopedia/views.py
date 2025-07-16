48d8547f33a0128ca851d022183c0e3d8504397c
Said <baraoudev@gmail.com>
2025-07-16 23:07:03 +0200

    fix: import redirect from django.shortcut and not from django itself

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,6 +1,6 @@
 # from django.http import HttpResponse
-from django.shortcuts import render
-import markdown, random, redirect
+from django.shortcuts import render, redirect
+import markdown, random
 from django.http import Http404
 from . import util
 
